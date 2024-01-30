a5c65a3f745d5d87db54061ae87965057093f848
Daisuke Sato <daisukjp1@gmail.com>
2024-01-29 22:34:13 -0700

    feat: sign-up form creation

--- memories-never-fade/src/_auth/forms/SignupForm.tsx
+++ memories-never-fade/src/_auth/forms/SignupForm.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
 
 import {
   Form,
@@ -14,9 +15,10 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignUpValidation } from "@/lib/validation";
 import { z } from "zod";
+import Loader from "@/components/shared/Loader";
 
 const SignupForm = () => {
-  const isLoading = true;
+  const isLoading = false;
   const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(SignUpValidation),
     defaultValues: {
@@ -28,17 +30,16 @@ const SignupForm = () => {
   });
 
   function onSubmit(values: z.infer<typeof SignUpValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // const newUser await createUserAccount(values);
     console.log(values);
   }
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold sm:pt-12">Create a new account</h2>
+      <div className="w-420 flex-center flex-col p-8 sm:p-0 transition-all ">
+        <img src="/assets/images/popyeg.png" alt="logo" />
+        <h2 className="h3-bold  pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use POPYEG enter your details
+          To use POPYEG, please enter your details
         </p>
 
         <form
@@ -103,11 +104,22 @@ const SignupForm = () => {
           />
           <Button type="submit" className="shad-button_primary">
             {isLoading ? (
-              <div className="flex-center gap-2">Loading...</div>
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
             ) : (
               "Sign up"
             )}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
